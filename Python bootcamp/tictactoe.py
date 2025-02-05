211ba9c274e3cb42e25535ffe98e6cc3231a77e6
jmitch09 <125391746+jmitch09@users.noreply.github.com>
2025-02-04 21:04:12 -0500

    Update tictactoe.py

--- Python bootcamp/tictactoe.py
+++ Python bootcamp/tictactoe.py
@@ -14,9 +14,9 @@ def initialize():
                         p1 = input("\nPlease enter X or O: ").upper()
 
         if p1 == 'X':
-                players = {'player1' : 'X', 'player2' : 'O'}
+                players = {'player1':'X','player2':'O'}
         else:
-                players = {'player1' : 'O', 'player2' : 'X'}
+                players = {'player1':'O','player2':'X'}
 
         return players, board
 
@@ -35,7 +35,7 @@ def display_board(board=None,choices=None,choice=None):
         print("-----------------")
         print("  {}  |  {}  |  {}  ".format(g,h,i))
 
-def user_selection(choices):
+def user_selection(choices,turn):
         choice = -1
         while choice not in range(9) and choice != 'E': 
                 choice = input("\nPlease enter 0-8 or E to end the game: ")
@@ -50,21 +50,30 @@ def user_selection(choices):
 
         #update choices with X or O at the index if choice is 0-8
         #return False if E else True
+        if choice == 'E':
+                return choice, choices, False
 
 
-        return choice, choices
+        return choice, choices, True
 
 def tic_tac_toe(players,board):
         play = True
         choices = [' '*9]
+        turn = 'player1'
         while play:
                 #ask user to select position (store position in list size 9, each index correlates to position on board) authenticate input
-                choice, play = user_selection(choices)
+                choice, choices, play = user_selection(choices,turn)
+                if play == False:
+                        return
                 #update player selection list and check if someone has won
                 #display new list and say who won or ask next player
                 display_board()
+                if turn == 'player1':
+                        turn = 'player2'
+                elif turn == 'player2':
+                        turn = 'player1'
 
-                #After win, ask to play again if yes clear data
+                #After win, ask to play again if yes clear data else return
 
 def main():
         #game setup (dictionary to remember p1 as x/o and p2 as x/o) dictionary mapping a-i to 0-8
